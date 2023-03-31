b644b0b149e17e53888fb895d827bcb6bbd0cd0a
Ayush Priyadarshi <ayush.priyadarshi2021@vitstudent.ac.in>
2023-03-31 11:39:18 +0530

    Final Commit

---
+++ index.js
@@ -0,0 +1,71 @@
+// let headChange=document.getElementsByClassName("leftHead");
+// let textChange=document.getElementsByClassName("leftContent");
+// let imgChange=document.getElementsByClassName("img3");
+
+
+console.log('Hi');
+
+document.querySelector("#brainStorm").onclick = function () {
+
+    const headChange = document.querySelector("#headChange");
+    headChange.innerText = "Brainstorming";
+    const contentChange = document.querySelector("#leftContent1");
+    contentChange.innerText = "Unleash creative ideas and build\n on them with the help of sticky \n notes, images, mind maps, videos, \n drawing capabilities — the list goes \n on."
+    const imgChange =document.querySelector("#img3");
+    imgChange.src="images/brainstorming.png";
+    
+}
+document.querySelector("#DiaGram").onclick = function () {
+
+    const headChange = document.querySelector("#headChange");
+    headChange.innerText = "Diagramming";
+    const contentChange = document.querySelector("#leftContent1");
+    contentChange.innerText = "Flowcharts, Swimlanes, Strategic \n Planning — tap into one of our \n many diagrams to visualize \n projects and ideas of any scope."
+    const imgChange =document.querySelector("#img3");
+    imgChange.src="images/diagramming.png";
+}
+document.querySelector("#MeetWork").onclick = function () {
+
+    const headChange = document.querySelector("#headChange");
+    headChange.innerText = "Meetings & Workshops";
+    const contentChange = document.querySelector("#leftContent1");
+    contentChange.innerText = "Give everyone in the room — even \n if they're miles away — a chance to \n connect and interact with sticky \n notes, voting, live reactions, and \n icebreakers."
+    const imgChange =document.querySelector("#img3");
+    imgChange.src="images/meeting.png";
+}
+document.querySelector("#ScrumEvent").onclick = function () {
+
+    const headChange = document.querySelector("#headChange");
+    headChange.innerText = "Scrum Events";
+    const contentChange = document.querySelector("#leftContent1");
+    contentChange.innerText = "Whether agile teams meet on a \n video call or sit in the same room, \n they can meet in Miro. Put ideas \n into action, and visualize projects \n and dependencies, all in one place."
+    const imgChange =document.querySelector("#img3");
+    imgChange.src="images/scrum events.png";
+}
+document.querySelector("#MaP").onclick = function () {
+
+    const headChange = document.querySelector("#headChange");
+    headChange.innerText = "Mapping";
+    const contentChange = document.querySelector("#leftContent1");
+    contentChange.innerText = "Plan projects, map out information \n architecture, or create org charts \n — you can plot it all in minutes on \n a Miro board."
+    const imgChange =document.querySelector("#img3");
+    imgChange.src="images/mapping.png";
+}
+document.querySelector("#RandD").onclick = function () {
+
+    const headChange = document.querySelector("#headChange");
+    headChange.innerText = "Research & Design";
+    const contentChange = document.querySelector("#leftContent1");
+    contentChange.innerText = "Foster a customer-centric mindset \n and build a shared team space, \n where everyone can capture \n insights, structure them with \n diagrams and tables, and share it \n all in a central spot."
+    const imgChange =document.querySelector("#img3");
+    imgChange.src="images/research.png";
+}
+document.querySelector("#SandP").onclick = function () {
+
+    const headChange = document.querySelector("#headChange");
+    headChange.innerText = "Strategic Planning";
+    const contentChange = document.querySelector("#leftContent1");
+    contentChange.innerText = "Define goals and initiatives, \n visualize priorities and outcomes, \n and watch those plans pay off in a \n big way. It's easier when you never \n run out of space."
+    const imgChange =document.querySelector("#img3");
+    imgChange.src="images/strategic planning.png";
+}
